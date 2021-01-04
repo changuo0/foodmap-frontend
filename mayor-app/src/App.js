6f7812afd5db61c005049d54a564b68c60a76929
caleb dubow <cdubowdeveloper@gmail.com>
2021-01-03 20:44:40 -0500

    Worked a bit on navBar

--- mayor-app/src/App.js
+++ mayor-app/src/App.js
@@ -5,6 +5,13 @@ class App extends Component{
   render() {
     return(
       <div className="Container">
+        <nav className="NavBar">
+            <a className="NavBarLink" href={"INSERT LINK LATER"}>Nashville Food</a>
+            <div className="NavBar-Expanded">
+              <input type="text"/>
+            </div>
+            
+        </nav>
         <h1 className="display-3">Find Food</h1>
         <p>Take this survey to find food near you</p>
         <button className="ButtonPrimary" On>Take Survey »</button>
