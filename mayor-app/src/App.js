f397128da128aea462c7b7713002a93331fe6f1d
chenhaoma18 <chenhao.ma@vanderbilt.edu>
2021-01-04 16:05:41 -0500

    started on assistance programs, added buttons, also added directory for images

--- mayor-app/src/App.js
+++ mayor-app/src/App.js
@@ -2,6 +2,10 @@ import { Component } from 'react';
 import './App.css';
 
 class App extends Component{
+    openUrl = (url) =>{
+        window.open(url);
+    }
+
   render() {
     return(
       <div className="Container">
@@ -10,12 +14,30 @@ class App extends Component{
             <div className="NavBar-Expanded">
               <input type="text"/>
             </div>
-            
         </nav>
+
+          {/*First Main Section  */}
         <h1 className="display-3">Find Food</h1>
         <p>Take this survey to find food near you</p>
         <button className="ButtonPrimary" On>Take Survey »</button>
+
+          {/* Assistance Programs section */}
+        <div className = "Container">
+            <h1 className = "SubDisplay">
+                Assistance Programs
+            </h1>
+
+            {/*maybe need to make url variables */}
+            <button className="ButtonPrimary" On onClick={() => this.openUrl("https://faonlineapp.dhs.tn.gov/")}>Apply Here »</button>
+
+            <button className="ButtonPrimary" On onClick={() => this.openUrl("https://www.tn.gov/health/health-program-areas/fhw/wic.html")}>Learn How to Apply »</button>
+
+            <button className="ButtonPrimary" On>Learn More »</button>
+
+        </div>
+
       </div>
+
 
     );
 
