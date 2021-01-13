de3cfb54a2afa3140f2bf924f591f328d063d312
caleb dubow <cdubowdeveloper@gmail.com>
2021-01-13 17:08:22 -0500

    Worked on navBar

--- mayor-app/src/components/Navigation.js
+++ mayor-app/src/components/Navigation.js
@@ -8,22 +8,34 @@ const Navigation = () => {
         <div>
             <nav className="NavBar">
                 {/*Links to homepage when on another page - can play around with styling*/}
-                <Link to="/" className = "btn">Nashville Food</Link>
+                {/*<Link to="/" className = "NavBarLink">Nashville Food</Link>*/}
+                <a className="NavBarLink" href={"/"}>Nashville Food</a>
                 <div className="NavBar-Expanded">
-                    <a className="NavBarItem" href={"INSERT LINK LATER"}>About</a>
-                    <Dropdown >
-                        <Dropdown.Toggle variant="NavBarItem-Dropdown">
-                            Get Help
-                        </Dropdown.Toggle>
-                        <Dropdown.Menu>
-                            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                    <a className="NavBarItem-Dropdown" href={"INSERT LINK LATER"}>Get Involved</a>
-                    <button className="SearchBarButton">Search</button>
+                    <ul className="NavBar-Nav mr-auto" >
+                        <a className="NavBarItem" href={"INSERT LINK LATER"}>About</a>
+                        <Dropdown>
+                            <Dropdown.Toggle variant="NavBarItem-Dropdown" drop={"down"}>
+                                Get Help
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                        <Dropdown >
+                            <Dropdown.Toggle variant="NavBarItem-Dropdown">
+                                Get Involved
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </ul>
                     <input className="SearchBar" type="text" placeholder="Search"/>
+                    <button className="SearchBarButton">Search</button>
                 </div>
             </nav>
         </div>
