f2a31b17f411c49d2c46936c1695981dcadd6a4a
caleb dubow <cdubowdeveloper@gmail.com>
2021-06-06 17:57:53 -0400

    Backend connected :)))))

--- mayor-app/src/components/Results.js
+++ mayor-app/src/components/Results.js
@@ -1,32 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Styling/App.css';
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 import Geocode from "react-geocode"
+import Tabletop from 'tabletop';
 
-function makeRequest(sheetOrCal,params,callback) {
-  const req = new XMLHttpRequest()
-  req.addEventListener("load", () => {
-    callback(JSON.parse(req.responseText))
-  })
-  let url = "/" + sheetOrCal + "/"
-  for (var k in params)
-    url += k + "=" + params[k] + "/"
-  req.open("GET", url)
-  req.send()
-}
-
-function makeDummyRequest(sheetOrCal,params,callback) {
-  // sheetOrCal and params do nothing lol
-  // calls the callback with sample data
-  callback([ { "name": "bla bla bla resource 1", "description": "...", "contact": "website/contact info goes here", "type": "Food Pantry", "address": "2301 vanderbilt place nashville tn", "zip": 37240, "latitude": 36.146, "longitude": -86.8050, "deliveryServices": true }, { "name": "...", "description": "...", "contact": "...", "type": "Ready to Eat", "address": "...", "zip": 12345, "latitude": 34.56, "longitude": -24.283, "deliveryServices": false }])
-}
 
 class Results extends React.Component {
     constructor() {
       super()
       this.setUpGeoCode();
       this.state = {
-        xmlReturn:[ { "name": "bla bla bla resource 1", "description": "...", "contact": "website/contact info goes here", "type": "Food Pantry", "address": "2301 vanderbilt place nashville tn", "zip": 37240, "latitude": 36.1486, "longitude": -86.8050, "deliveryServices": true }, { "name": "...", "description": "...", "contact": "...", "type": "Ready to Eat", "address": "...", "zip": 12345, "latitude": 0, "longitude":0, "deliveryServices": false }],
+
+        resourceData: [],
+        validRecources: [],
+        hasLoaded: false,
+        activeMarker: {},
+        selectedPlace: {},
+        showingInfoWindow: false,
+
         householdProperties: {
             olderThan60: false,
             childrenUnder18: false,
@@ -55,16 +46,46 @@ class Results extends React.Component {
           availableLocations: []
       }
       
-      makeDummyRequest("sheet",{"zip":"37206"}, (x) => this.setState({xmlReturn:x}))
     }
-    
+
+    onMarkerClick = (props, marker) =>
+      this.setState({
+        activeMarker: marker,
+        selectedPlace: props,
+        showingInfoWindow: true
+      });
+
+    onInfoWindowClose = () =>
+        this.setState({
+        activeMarker: null,
+        showingInfoWindow: false
+      });
+
+    onMapClicked = () => {
+      if (this.state.showingInfoWindow)
+        this.setState({
+          activeMarker: null,
+          showingInfoWindow: false
+        });
+      };
+
+    //Get data from survey and google sheets
     componentDidMount(){
-        this.setState(prevState => ({
-          householdProperties: JSON.parse(localStorage.getItem('householdProperties')),
-          desiredFoodType: JSON.parse(localStorage.getItem('desiredFoodType')),
-          locationDetails: JSON.parse(localStorage.getItem('locationDetails')),
-          weeklyAvailability: JSON.parse(localStorage.getItem('weeklyAvailability'))
-        }))
+        Tabletop.init({
+          key: '1XrmXfbWx_oWC0sqOIdfZzPFIMinkiiijyUAKy2FUXw4',
+          callback: googleData => {
+            this.setState(prevState => ({
+              resourceData: googleData,
+              householdProperties: JSON.parse(localStorage.getItem('householdProperties')),
+              desiredFoodType: JSON.parse(localStorage.getItem('desiredFoodType')),
+              locationDetails: JSON.parse(localStorage.getItem('locationDetails')),
+              weeklyAvailability: JSON.parse(localStorage.getItem('weeklyAvailability')),
+              hasLoaded: true
+            }))
+            console.log('google sheet data --->', googleData)
+          },
+          simpleSheet: true
+        })
         
     }
 
@@ -74,23 +95,52 @@ class Results extends React.Component {
 
     
     render() {
+      const {resourceData} = this.state
+      if (!this.state.hasLoaded){
+        return <div />
+      }
+
+
         return (
             <div>
                 {/*First Main Section  */}
                 <div className="Container">
                     <h1 className="display-3">Results</h1>
-		                <p>{JSON.stringify(this.state.xmlReturn)}</p>
-                    
-                    <p>state: {this.state.locationDetails.locationRadius}</p>
+		                
                     <Map
                         google={this.props.google}
                         zoom={15}
                         style={mapStyles}
                         
-                        //initialCenter={{ lat: "36.1486", lng:  "-86.8050"}}
-                        initialCenter={{ lat: this.state.xmlReturn[0].latitude, lng: this.state.xmlReturn[0].longitude}}
+                        
+                        initialCenter={{lat: resourceData[0].Lat, lng: resourceData[0].Lng}}
                     >  
-                    <Marker position={{ lat: this.state.xmlReturn[0].latitude, lng: this.state.xmlReturn[0].longitude}} />
+                    {
+                        resourceData.map((resource) => (
+                            <Marker
+                              name={resource.Name}
+                              description={resource.Description}
+                              address={resource.Address}
+                              email={resource.Email}
+                              resourceType={resource.Type}
+                              foodType={resource.FoodType}
+                              position={{lat: resource.Lat, lng: resource.Lng}} 
+                              onClick={this.onMarkerClick}
+                            />
+                        ))
+                    }
+                    <InfoWindow
+                      marker={this.state.activeMarker}
+                      onClose={this.onInfoWindowClose}
+                      visible={this.state.showingInfoWindow}
+                    >
+                      <div>
+                        <h4>{this.state.selectedPlace.name}</h4>
+                      </div>
+                    </InfoWindow>
+
+                      
+                    
                     </Map>
                 </div>
                 <p>{this.state.householdProperties.olderThan60}</p>
@@ -99,9 +149,24 @@ class Results extends React.Component {
     }
 
 
+
   setUpGeoCode(){
     Geocode.setApiKey("AIzaSyAZNY02jemZ0JuL9QauPxeggJB7EDShTo8");
     Geocode.setLanguage("en");
+  }
+
+  addMarkers(){
+    const {resourceData} = this.state
+    return (
+      <div> {
+          resourceData.map(resource => {
+
+
+
+          })
+        }
+      </div>
+    )
   }
 }
 const mapStyles = {
